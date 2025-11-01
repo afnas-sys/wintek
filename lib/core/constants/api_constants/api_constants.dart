1ca81fb2fbc3a93da87baa718388e257ab9b45ca
Vasil PM IT Solution <vasil@thepmitsolution.com>
2025-11-01 18:19:04 +0530

    update user profile

--- lib/core/constants/api_constants/api_constants.dart
+++ lib/core/constants/api_constants/api_constants.dart
@@ -2,4 +2,5 @@ class ApiContants {
   static const String baseUrl = 'https://ank143matkaapp.info/api/v1/';
   static const String changePasswordUrl = '${baseUrl}app/users/changepass';
   static const String fetchUserUrl = '${baseUrl}app/users/get/';
+  static const String updateProfileUrl = '${baseUrl}app/users/update';
 }
